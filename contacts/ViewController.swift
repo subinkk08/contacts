92862f1cff97b62440c82be62101c71f67b459b2
Subin K Kuriakose <subin.kuriakose@bs-shipmanagement.com>
2016-04-24 23:10:59 +0530

    Initial Commit

---
+++ contacts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  contacts
+//
+//  Created by SUBIN K KURIAKOSE on 24/04/16.
+//  Copyright © 2016 SUBIN K KURIAKOSE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
